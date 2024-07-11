86673a6d5eb98bc617a67dd48f33fadfcf1d272b
riacosta <rigo93acosta@gmail.com>
2024-07-11 02:30:10 +0000

    Init project

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+//##### Defines #####
+
+//##### Public objects #####
+
+//##### Public global variables #####
+
+//##### Declaration public functions #####
+
+
+
+//##### Main #####
+int main()
+{
+    while (true) {
+
+    }
+}
+
+//##### Implemention public function #####
+
