9e2fa93d95005c72b0484f299bf2c4917f369840
riacosta <rigo93acosta@gmail.com>
2024-07-11 13:16:18 +0000

    Add code for LM35

--- main.cpp
+++ main.cpp
@@ -6,24 +6,41 @@
 //##### Public objects #####
 DigitalInOut sirenPin(D8);
 
+AnalogIn sensorLM35(A0);
+
+UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
+
 //##### Public global variables #####
+float temperatureLM35 = 0;
 
 //##### Declaration public functions #####
-
-
+void init();
+void actionBuzzer();
 
 //##### Main #####
 int main()
 {   
-    sirenPin.mode(OpenDrain);
-    sirenPin.input();
+    char str[100];
+    int stringLength;
     while (true) {
-        sirenPin.output();
-        sirenPin = LOW;
-        delay(2000);
-        sirenPin.input();
+        temperatureLM35 = sensorLM35.read();
+        sprintf(str, "Temperature: %.2f \xB0 C\r\n", temperatureLM35);
+        stringLength = strlen(str);
+        uartUsb.write(str, stringLength);
+        delay(1000);
     }
 }
 
 //##### Implemention public function #####
 
+void init(){
+    sirenPin.mode(OpenDrain);
+    sirenPin.input();
+}
+
+void actionBuzzer(){
+    sirenPin.output();
+    sirenPin = LOW;
+    delay(2000);
+    sirenPin.input();
+}
