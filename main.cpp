75c1ec1e35457ba2dfec585cc9773698f7474085
riacosta <rigo93acosta@gmail.com>
2024-07-11 13:20:10 +0000

    Add uartTx

--- main.cpp
+++ main.cpp
@@ -16,17 +16,15 @@ float temperatureLM35 = 0;
 //##### Declaration public functions #####
 void init();
 void actionBuzzer();
+void uartTx(float value);
 
 //##### Main #####
 int main()
 {   
-    char str[100];
-    int stringLength;
+
     while (true) {
         temperatureLM35 = sensorLM35.read();
-        sprintf(str, "Temperature: %.2f \xB0 C\r\n", temperatureLM35);
-        stringLength = strlen(str);
-        uartUsb.write(str, stringLength);
+        uartTx(temperatureLM35);
         delay(1000);
     }
 }
@@ -43,4 +41,12 @@ void actionBuzzer(){
     sirenPin = LOW;
     delay(2000);
     sirenPin.input();
+}
+
+void uartTx(float value){
+    char str[100];
+    int stringLength;
+    sprintf(str, "Temperature: %.2f C\r\n", value);
+    stringLength = strlen(str);
+    uartUsb.write(str, stringLength);
 }
