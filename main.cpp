6e22780cccab6d9ba42c22688077e66b09b208d7
riacosta <rigo93acosta@gmail.com>
2024-07-11 14:38:41 +0000

    Add averageTemp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include "arm_book_lib.h"
 
 //##### Defines #####
+#define NUMBER_OF_AVG_SAMPLES 10
+#define OVER_TEMP_LEVEL 50
+
+#define POTENTIOMETER_OVER_GAS_LEVEL 0.5
+
 
 //##### Public objects #####
 DigitalInOut sirenPin(D8);
@@ -17,7 +22,12 @@ AnalogIn alarmGas(A1); //Empleo Potenciometro no tengo ese sensor
 UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
 
 //##### Public global variables #####
-float temperatureLM35 = 0;
+int lm35SampleIndex = 0;
+float lm35ReadingsArray [NUMBER_OF_AVG_SAMPLES];
+float lm35ReadingsSum = 0.0;
+float lm35ReadingsAverage = 0.0;
+float lm35TempC = 0.0;
+
 float valueGas = 0;  //Empleo Potenciometro no tengo ese sensor
 
 //##### Declaration public functions #####
@@ -27,18 +37,28 @@ void actionBuzzer();
 void uartTx(float value, int flag);
 void testAlarm();
 
+float analogReadingScaledWithTheLM35Formula( float analogReading );
+float celsiusToFahrenheit( float tempInCelsiusDegrees );
+
+void lm35ReadingsArrayInit();
+void readTemperature();
+
 //##### Main #####
 int main()
 {   
 
+    inputsInit();
+    outputsInit();
+    void lm35ReadingsArrayInit();
+
     while (true) {
         testAlarm();
-        temperatureLM35 = sensorLM35.read();
-        uartTx(temperatureLM35, 1);
-        delay(1000);
+        readTemperature();
+        uartTx(lm35TempC, 1);
+        thread_sleep_for(1000);
         valueGas = alarmGas.read();
         uartTx(valueGas, 2);
-        delay(1000);
+        thread_sleep_for(1000);
     }
 }
 
@@ -63,7 +83,7 @@ void testAlarm(){
 void actionBuzzer(){
     sirenPin.output();
     sirenPin = LOW;
-    delay(1000);
+    thread_sleep_for(1000);
     sirenPin.input();
 }
 
@@ -75,16 +95,12 @@ void uartTx(float value, int flag){
             sprintf(str, "Temperature: %.2f C\r\n", value);
             stringLength = strlen(str);
             uartUsb.write(str, stringLength);
-            incorrectLed = ON;
-            systemBlockedLed = OFF;
         break;
 
         case 2: //Flag 2 Gas
             sprintf(str, "Gas: %.2f C\r\n", value);
             stringLength = strlen(str);
             uartUsb.write(str, stringLength);
-            incorrectLed = OFF;
-            systemBlockedLed = ON;
             break;
 
         default:
@@ -94,4 +110,37 @@ void uartTx(float value, int flag){
             break;
 
     }        
+}
+
+void readTemperature(){
+
+    static int lm35SampleIndex = 0;
+    lm35ReadingsArray[lm35SampleIndex] = sensorLM35.read();
+    lm35SampleIndex++;
+    if (lm35SampleIndex >= NUMBER_OF_AVG_SAMPLES) {
+      lm35SampleIndex = 0;
+    }
+
+    lm35ReadingsSum = 0.0;
+    for (int i = 0; i < NUMBER_OF_AVG_SAMPLES; i++) {
+      lm35ReadingsSum = lm35ReadingsSum + lm35ReadingsArray[i];
+    }
+    lm35ReadingsAverage = lm35ReadingsSum / NUMBER_OF_AVG_SAMPLES;
+    lm35TempC = analogReadingScaledWithTheLM35Formula ( lm35ReadingsAverage );
+}
+
+
+float analogReadingScaledWithTheLM35Formula(float analogReading) {
+  return (analogReading * 3.3 / 0.01);
+}
+
+float celsiusToFahrenheit(float tempInCelsiusDegrees) {
+  return (tempInCelsiusDegrees * 9.0 / 5.0 + 32.0);
+}
+
+void lm35ReadingsArrayInit() {
+  int i;
+  for (i = 0; i < NUMBER_OF_AVG_SAMPLES; i++) {
+    lm35ReadingsArray[i] = 0;
+  }
 }
